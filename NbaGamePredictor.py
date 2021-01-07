fd259ab2fff29bbe008ab57754a497e97200b16e
brianschroeder <54692447+brianschroeder@users.noreply.github.com>
2021-01-07 01:08:37 -0500

    Update NbaGamePredictor.py

--- NbaGamePredictor.py
+++ NbaGamePredictor.py
@@ -1,6 +1,6 @@
 # Projects Plans for NBA Game Predictor
-# Get All Games for the Day - Done
-# Get Teams Stats for Home and Away Teams and put them in a Table - Done
+# Get All Games for the Day
+# Get Teams Stats for Home and Away Teams and put them in a Table
 # Compare teams FG%,PPG,FG% Defense,Assist,Turnovers
 
 import requests
@@ -39,31 +39,54 @@ def GameAnalysis():
         else: 
             awayAdvantage += 1
         if ((TeamStats(homeTeam))['points'][0])/homeGamesPlayed > ((TeamStats(awayTeam))['points'][0])/awayGamesPlayed:
-            homeAdvantage += 1
+            homeAdvantage += 2
         else: 
-            awayAdvantage += 1
+            awayAdvantage += 2
         if ((TeamStats(homeTeam))['field_goal_percentage'][0]) > ((TeamStats(awayTeam))['field_goal_percentage'][0]):
-            homeAdvantage += 1
+            homeAdvantage += 3
         else: 
-            awayAdvantage += 1
+            awayAdvantage += 3
         if ((TeamStats(homeTeam))['turnovers'][0])/homeGamesPlayed < ((TeamStats(awayTeam))['turnovers'][0])/awayGamesPlayed:
             homeAdvantage += 1
         else: 
             awayAdvantage += 1
         if ((TeamStats(homeTeam))['opp_points'][0])/homeGamesPlayed < ((TeamStats(awayTeam))['opp_points'][0])/awayGamesPlayed:
-            homeAdvantage += 1
+            homeAdvantage += 2
         else: 
-            awayAdvantage += 1
+            awayAdvantage += 2
         if ((TeamStats(homeTeam))['opp_field_goal_percentage'][0]) < ((TeamStats(awayTeam))['opp_field_goal_percentage'][0]):
-            homeAdvantage += 1
+            homeAdvantage += 3
         else: 
-            awayAdvantage += 1
+            awayAdvantage += 3
         if ((TeamStats(homeTeam))['opp_assists'][0])/homeGamesPlayed < ((TeamStats(awayTeam))['opp_assists'][0])/awayGamesPlayed:
             homeAdvantage += 1
         else: 
             awayAdvantage += 1
-        print(homeTeam + ' has a ' + str(homeAdvantage) + ' point advantage over ' + awayTeam)
-        print(awayTeam + ' has a ' + str(awayAdvantage) + ' point advantage over ' + homeTeam)
-       
+        #print(homeTeam + ' has a ' + str(homeAdvantage) + ' point advantage over ' + awayTeam)
+        #print(awayTeam + ' has a ' + str(awayAdvantage) + ' point advantage over ' + homeTeam)
 
+        stats = {
+            'Home Team': [homeTeam],
+            'Home PPG': [((TeamStats(homeTeam))['points'][0])/homeGamesPlayed],
+            'Home Assists P/G' : [((TeamStats(homeTeam))['assists'][0])/homeGamesPlayed],
+            'Home FG%': [((TeamStats(homeTeam))['field_goal_percentage'][0])],
+            'Home Turnovers P/G': [((TeamStats(homeTeam))['turnovers'][0])/homeGamesPlayed], 
+            'Home Allowed PPG': [((TeamStats(homeTeam))['opp_points'][0])/homeGamesPlayed],
+            'Home Allowed FG%': [((TeamStats(homeTeam))['opp_field_goal_percentage'][0])],
+            'Home Allowed Assist P/G': [((TeamStats(homeTeam))['opp_assists'][0])/homeGamesPlayed],
+            'Away Team': [awayTeam],
+            'Away PPG': [((TeamStats(awayTeam))['points'][0])/awayGamesPlayed],
+            'Away Assists P/G' : [((TeamStats(awayTeam))['assists'][0])/awayGamesPlayed],
+            'Away FG%': [((TeamStats(awayTeam))['field_goal_percentage'][0])],
+            'Away Turnovers P/G': [((TeamStats(awayTeam))['turnovers'][0])/awayGamesPlayed], 
+            'Away Allowed PPG': [((TeamStats(awayTeam))['opp_points'][0])/awayGamesPlayed],
+            'Away Allowed FG%': [((TeamStats(awayTeam))['opp_field_goal_percentage'][0])],
+            'Away Allowed Assist P/G': [((TeamStats(awayTeam))['opp_assists'][0])/awayGamesPlayed],
+            'Home Team Advantage': [homeAdvantage],
+            'Away Team Advantage': [awayAdvantage]
+        }
+
+        statsdf = pd.DataFrame(data = stats)
+        print(statsdf)
+        
 GameAnalysis()
