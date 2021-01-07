4bff63cbdfcbd05459c9f1898b2d94e8951ec52b
brianschroeder <54692447+brianschroeder@users.noreply.github.com>
2021-01-06 23:04:22 -0500

    Update NbaGamePredictor.py

--- NbaGamePredictor.py
+++ NbaGamePredictor.py
@@ -1,55 +1,34 @@
 # Projects Plans for NBA Game Predictor
 # Get All Games for the Day - Done
-# Get Teams Stats for Home and Away Teams and put them in a Table
+# Get Teams Stats for Home and Away Teams and put them in a Table - Done
 # Compare teams FG%,PPG,FG% Defense,Assist,Turnovers
 
 import requests
 import json
 import pandas as pd
+from sportsreference.nba.teams import Teams
 
-def nbaschedule(showschedule):
-    hometeam = []
-    hometeamID = []
-    hometeamwins = []
-    hometeamlosses = []
-    awayteam = []
-    awayteamID = []
-    awayteamwins = []
-    awayteamlosses = []
-    todaysgames = {}
-    teamIDs = {} 
+def nbaschedule():
+    matchups = {}
+    hometeams = []
+    awayteams = [] 
     request = requests.get('https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json').text
     request_json = json.loads(request)
     scoreboard = (request_json['scoreboard']['games'])
     for game in scoreboard:
-        hometeam.append(game['homeTeam']['teamName'])
-        awayteam.append(game['awayTeam']['teamName'])
-        hometeamwins.append(game['homeTeam']['wins'])
-        awayteamwins.append(game['awayTeam']['wins'])
-        hometeamlosses.append(game['homeTeam']['losses'])
-        awayteamlosses.append(game['awayTeam']['losses'])
-        hometeamID.append(game['homeTeam']['teamId'])
-        awayteamID.append(game['awayTeam']['teamId'])
-    todaysgames.update({'Home Team': hometeam})
-    todaysgames.update({'Home Team Wins': hometeamwins})
-    todaysgames.update({'Home Team Losses': hometeamlosses})
-    todaysgames.update({'Away Team': awayteam})
-    todaysgames.update({'Away Team Wins': awayteamwins})
-    todaysgames.update({'Away Team Losses': awayteamlosses})
-    teamIDs.update({'Home Team ID': hometeamID})
-    teamIDs.update({'Away Team ID': awayteamID})
-    df = pd.DataFrame(data=todaysgames)
-    if showschedule == True:
-        print(df.sort_values(by=['Home Team Wins'], ascending=False))
-    return teamIDs
+        hometeams.append(game['homeTeam']['teamCity'] + ' ' + game['homeTeam']['teamName'])
+        awayteams.append(game['awayTeam']['teamCity'] + ' ' + game['awayTeam']['teamName'])
+    matchups.update({'Home Team': hometeams})
+    matchups.update({'Away Team': awayteams})
+    return matchups
 
+def teamstats(teamname):
+    teams = Teams()
+    for team in teams: 
+        df = pd.DataFrame(data=team.dataframe)
+        stats = (df[['name', 'games_played', 'points', 'assists', 'turnovers', 'field_goal_percentage', 'rank', 'opp_points', 'opp_field_goal_percentage', 'opp_assists']])
+        if stats.name[0] == teamname:
+            return stats
 
-def teamstats():
-    request = requests.get('https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json').text
-    request_json = json.loads(request)
-    scoreboard = (request_json['scoreboard']['games'])
-    for game in scoreboard:
-        print(game['homeTeam']['teamId'])
-        print(game['awayTeam']['teamId'])
-        print('New Game')
-teamstats()
+print(nbaschedule())
+     
