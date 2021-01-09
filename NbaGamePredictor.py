77ba7e5fd4c571251820209ca5d79c59680ba847
brianschroeder <54692447+brianschroeder@users.noreply.github.com>
2021-01-09 12:34:25 -0500

    Update NbaGamePredictor.py

--- NbaGamePredictor.py
+++ NbaGamePredictor.py
@@ -45,81 +45,46 @@ def TeamRatings(teamname):
 def GameAnalysis():
     all_stats = []
     for matchup in NbaSchedule():
-        homeAdvantage = 0
-        awayAdvantage = 0
         homeTeam = (matchup.split(':')[0]).replace('LA', 'Los Angeles')
         awayTeam = (matchup.split(':')[1]).replace('LA', 'Los Angeles')
         homeGamesPlayed = (TeamStats(homeTeam))['games_played'][0]
         awayGamesPlayed = (TeamStats(awayTeam))['games_played'][0]
+        home_defense_rating = float((TeamRatings(homeTeam)['DRtg/A'].to_string()).split('   ')[1])
+        away_defense_rating = float((TeamRatings(awayTeam)['DRtg/A'].to_string()).split('   ')[1])
+        home_offense_rating = float((TeamRatings(homeTeam)['ORtg/A'].to_string()).split('   ')[1])
+        away_offense_rating = float((TeamRatings(awayTeam)['ORtg/A'].to_string()).split('   ')[1])
     
-        if ((TeamStats(homeTeam))['points'][0])/homeGamesPlayed > ((TeamStats(homeTeam))['opp_points'][0])/homeGamesPlayed:
-            homeAdvantage += 4
+        if home_defense_rating < away_defense_rating:
+            home_dr_advantage = away_defense_rating - home_defense_rating 
+            away_dr_advantage = home_defense_rating - away_defense_rating 
+        else:
+            away_dr_advantage = home_defense_rating - away_defense_rating
+            home_dr_advantage = away_defense_rating - home_defense_rating 
         
-        if ((TeamStats(awayTeam))['points'][0])/awayGamesPlayed > ((TeamStats(awayTeam))['opp_points'][0])/awayGamesPlayed:
-            awayAdvantage += 4
-        
-        if ((TeamStats(homeTeam))['assists'][0])/homeGamesPlayed > ((TeamStats(awayTeam))['assists'][0])/awayGamesPlayed:
-            homeAdvantage += 1
-        else: 
-            awayAdvantage += 1
-        if ((TeamStats(homeTeam))['points'][0])/homeGamesPlayed > ((TeamStats(awayTeam))['points'][0])/awayGamesPlayed:
-            homeAdvantage += 2
-        else: 
-            awayAdvantage += 2
-        if ((TeamStats(homeTeam))['field_goal_percentage'][0]) > ((TeamStats(awayTeam))['field_goal_percentage'][0]):
-            homeAdvantage += 3
-        else: 
-            awayAdvantage += 3
-        if ((TeamStats(homeTeam))['turnovers'][0])/homeGamesPlayed < ((TeamStats(awayTeam))['turnovers'][0])/awayGamesPlayed:
-            homeAdvantage += 1
-        else: 
-            awayAdvantage += 1
-        if ((TeamStats(homeTeam))['opp_points'][0])/homeGamesPlayed < ((TeamStats(awayTeam))['opp_points'][0])/awayGamesPlayed:
-            homeAdvantage += 2
-        else: 
-            awayAdvantage += 2
-        if ((TeamStats(homeTeam))['opp_field_goal_percentage'][0]) < ((TeamStats(awayTeam))['opp_field_goal_percentage'][0]):
-            homeAdvantage += 3
-        else: 
-            awayAdvantage += 3
-        if ((TeamStats(homeTeam))['opp_assists'][0])/homeGamesPlayed < ((TeamStats(awayTeam))['opp_assists'][0])/awayGamesPlayed:
-            homeAdvantage += 1
-        else: 
-            awayAdvantage += 1
-        #print(homeTeam + ' has a ' + str(homeAdvantage) + ' point advantage over ' + awayTeam)
-        #print(awayTeam + ' has a ' + str(awayAdvantage) + ' point advantage over ' + homeTeam)
-
+        if home_offense_rating > away_offense_rating:
+            home_or_advantage = home_offense_rating - away_offense_rating
+            away_or_advantage = away_offense_rating - home_offense_rating
+        else:
+            away_or_advantage = away_offense_rating - home_offense_rating
+            home_or_advantage = home_offense_rating - away_offense_rating
         stats = {
             'Home Team': homeTeam,
-            'Home PPG': ((TeamStats(homeTeam))['points'][0])/homeGamesPlayed,
-            'Home Assists P/G' : ((TeamStats(homeTeam))['assists'][0])/homeGamesPlayed,
-            'Home FG%': ((TeamStats(homeTeam))['field_goal_percentage'][0]),
-            'Home Turnovers P/G': ((TeamStats(homeTeam))['turnovers'][0])/homeGamesPlayed, 
-            'Home Allowed PPG': ((TeamStats(homeTeam))['opp_points'][0])/homeGamesPlayed,
-            'Home Allowed FG%': ((TeamStats(homeTeam))['opp_field_goal_percentage'][0]),
-            'Home Allowed Assist P/G': ((TeamStats(homeTeam))['opp_assists'][0])/homeGamesPlayed,
             'Away Team': awayTeam,
-            'Away PPG': ((TeamStats(awayTeam))['points'][0])/awayGamesPlayed,
-            'Away Assists P/G' : ((TeamStats(awayTeam))['assists'][0])/awayGamesPlayed,
+            'Home Team Defensive Rating Difference': home_dr_advantage,
+            'Away Team Defensive Rating Difference': away_dr_advantage,
+            'Home Team Offensive Rating Difference': home_or_advantage,
+            'Away Team Offensive Rating Difference': away_or_advantage,
+            'Home FG%': ((TeamStats(homeTeam))['field_goal_percentage'][0]),
             'Away FG%': ((TeamStats(awayTeam))['field_goal_percentage'][0]),
-            'Away Turnovers P/G': ((TeamStats(awayTeam))['turnovers'][0])/awayGamesPlayed, 
-            'Away Allowed PPG': ((TeamStats(awayTeam))['opp_points'][0])/awayGamesPlayed,
-            'Away Allowed FG%': ((TeamStats(awayTeam))['opp_field_goal_percentage'][0]),
-            'Away Allowed Assist P/G': ((TeamStats(awayTeam))['opp_assists'][0])/awayGamesPlayed,
-            'Home Team Advantage': homeAdvantage,
-            'Away Team Advantage': awayAdvantage,
-            'Home Team Defensive Rating' : (TeamRatings(homeTeam)['DRtg/A'].to_string()).split('   ')[1],
-            'Away Team Defensive Rating' : (TeamRatings(awayTeam)['DRtg/A'].to_string()).split('   ')[1],
-            'Home Team Ofensive Rating' : (TeamRatings(homeTeam)['ORtg/A'].to_string()).split('   ')[1],
-            'Away Team Ofensive Rating' : (TeamRatings(awayTeam)['ORtg/A'].to_string()).split('   ')[1],
-            'Home Team MOV/A' : (TeamRatings(homeTeam)['MOV/A'].to_string()).split('   ')[1],
-            'Away Team MOV/A' : (TeamRatings(awayTeam)['MOV/A'].to_string()).split('   ')[1]
+            'Home Assists P/G' : ((TeamStats(homeTeam))['assists'][0])/homeGamesPlayed,
+            'Away Assists P/G' : ((TeamStats(awayTeam))['assists'][0])/awayGamesPlayed,
+            'Home Turnovers P/G': ((TeamStats(homeTeam))['turnovers'][0])/homeGamesPlayed,
+            'Away Turnovers P/G': ((TeamStats(awayTeam))['turnovers'][0])/awayGamesPlayed
         }
 
         all_stats.append(stats)
     stats_dataframe = pd.DataFrame(data = all_stats)
-    stats_dataframe_sorted = stats_dataframe.sort_values(by='Home Team Advantage', ascending=False)
-    html = stats_dataframe_sorted.to_html(classes='table table-striped table-hover')
+    html = stats_dataframe.to_html(classes='table table-striped table-hover')
     html_file = open("index.html", "w")
     html_file.write(html)
     html_file.close()
