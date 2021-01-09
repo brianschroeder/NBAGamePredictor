a044b0b31eb940af84573d053ced0223f33dab2f
brianschroeder <54692447+brianschroeder@users.noreply.github.com>
2021-01-09 14:17:03 -0500

    Update NbaGamePredictor.py

--- NbaGamePredictor.py
+++ NbaGamePredictor.py
@@ -22,7 +22,7 @@ def TeamStats(teamname):
     teams = Teams()
     for team in teams: 
         df = pd.DataFrame(data=team.dataframe)
-        stats = (df[['name', 'games_played', 'points', 'assists', 'turnovers', 'field_goal_percentage', 'rank', 'opp_points', 'opp_field_goal_percentage', 'opp_assists']])
+        stats = (df[['name', 'games_played', 'points', 'assists', 'turnovers', 'field_goal_percentage', 'rank', 'opp_points', 'opp_field_goal_percentage', 'opp_assists', 'field_goal_attempts', 'total_rebounds', 'three_point_field_goal_percentage']])
         if stats.name[0] == teamname:
             return stats
 
@@ -53,7 +53,9 @@ def GameAnalysis():
         away_defense_rating = float((TeamRatings(awayTeam)['DRtg/A'].to_string()).split('   ')[1])
         home_offense_rating = float((TeamRatings(homeTeam)['ORtg/A'].to_string()).split('   ')[1])
         away_offense_rating = float((TeamRatings(awayTeam)['ORtg/A'].to_string()).split('   ')[1])
-    
+        home_assist_turnover_ratio = (((TeamStats(homeTeam))['assists'][0])/homeGamesPlayed)/(((TeamStats(homeTeam))['turnovers'][0])/homeGamesPlayed)
+        away_assist_turnover_ratio = (((TeamStats(awayTeam))['assists'][0])/awayGamesPlayed)/(((TeamStats(awayTeam))['turnovers'][0])/awayGamesPlayed)
+
         if home_defense_rating < away_defense_rating:
             home_dr_advantage = away_defense_rating - home_defense_rating 
             away_dr_advantage = home_defense_rating - away_defense_rating 
@@ -67,19 +69,59 @@ def GameAnalysis():
         else:
             away_or_advantage = away_offense_rating - home_offense_rating
             home_or_advantage = home_offense_rating - away_offense_rating
+
+        if home_assist_turnover_ratio > away_assist_turnover_ratio:
+            home_at_advantage = home_assist_turnover_ratio - away_assist_turnover_ratio
+            away_at_advantage = away_assist_turnover_ratio - home_assist_turnover_ratio
+        else:
+            away_at_advantage = away_assist_turnover_ratio - home_assist_turnover_ratio
+            home_at_advantage = home_assist_turnover_ratio - away_assist_turnover_ratio
+
+        if ((TeamStats(homeTeam))['total_rebounds'][0])/homeGamesPlayed > ((TeamStats(awayTeam))['total_rebounds'][0])/awayGamesPlayed:
+            home_rb_advantage = ((TeamStats(homeTeam))['total_rebounds'][0])/homeGamesPlayed - ((TeamStats(awayTeam))['total_rebounds'][0])/awayGamesPlayed
+            away_rb_advantage = ((TeamStats(awayTeam))['total_rebounds'][0])/awayGamesPlayed - ((TeamStats(homeTeam))['total_rebounds'][0])/homeGamesPlayed
+        else:
+            away_rb_advantage = ((TeamStats(awayTeam))['total_rebounds'][0])/awayGamesPlayed - ((TeamStats(homeTeam))['total_rebounds'][0])/homeGamesPlayed
+            home_rb_advantage = ((TeamStats(homeTeam))['total_rebounds'][0])/homeGamesPlayed - ((TeamStats(awayTeam))['total_rebounds'][0])/awayGamesPlayed
+        
+        if ((TeamStats(homeTeam))['three_point_field_goal_percentage'][0]) > ((TeamStats(awayTeam))['three_point_field_goal_percentage'][0]):
+            home_three_advantage = ((TeamStats(homeTeam))['three_point_field_goal_percentage'][0]) - ((TeamStats(awayTeam))['three_point_field_goal_percentage'][0])
+            away_three_advantage = ((TeamStats(awayTeam))['three_point_field_goal_percentage'][0]) - ((TeamStats(homeTeam))['three_point_field_goal_percentage'][0])
+        else:
+            away_three_advantage = ((TeamStats(awayTeam))['three_point_field_goal_percentage'][0]) - ((TeamStats(homeTeam))['three_point_field_goal_percentage'][0])
+            home_three_advantage = ((TeamStats(homeTeam))['three_point_field_goal_percentage'][0]) - ((TeamStats(awayTeam))['three_point_field_goal_percentage'][0])
+
+        if ((TeamStats(homeTeam))['field_goal_percentage'][0]) > ((TeamStats(awayTeam))['field_goal_percentage'][0]):
+            home_fg_advantage = ((TeamStats(homeTeam))['field_goal_percentage'][0]) - ((TeamStats(awayTeam))['field_goal_percentage'][0])
+            away_fg_advantage = ((TeamStats(awayTeam))['field_goal_percentage'][0]) - ((TeamStats(homeTeam))['field_goal_percentage'][0])
+        else:
+            away_fg_advantage = ((TeamStats(awayTeam))['field_goal_percentage'][0]) - ((TeamStats(homeTeam))['field_goal_percentage'][0])
+            home_fg_advantage = ((TeamStats(homeTeam))['field_goal_percentage'][0]) - ((TeamStats(awayTeam))['field_goal_percentage'][0])
+        
+        if ((TeamStats(homeTeam))['field_goal_attempts'][0])/homeGamesPlayed > ((TeamStats(awayTeam))['field_goal_attempts'][0])/awayGamesPlayed:
+            home_fga_advantage = ((TeamStats(homeTeam))['field_goal_attempts'][0])/homeGamesPlayed - ((TeamStats(awayTeam))['field_goal_attempts'][0])/awayGamesPlayed
+            away_fga_advantage = ((TeamStats(awayTeam))['field_goal_attempts'][0])/awayGamesPlayed - ((TeamStats(homeTeam))['field_goal_attempts'][0])/homeGamesPlayed
+        else:
+            away_fga_advantage = ((TeamStats(awayTeam))['field_goal_attempts'][0])/awayGamesPlayed - ((TeamStats(homeTeam))['field_goal_attempts'][0])/homeGamesPlayed
+            home_fga_advantage = ((TeamStats(homeTeam))['field_goal_attempts'][0])/homeGamesPlayed - ((TeamStats(awayTeam))['field_goal_attempts'][0])/awayGamesPlayed
+
         stats = {
             'Home Team': homeTeam,
             'Away Team': awayTeam,
-            'Home Team Defensive Rating Difference': home_dr_advantage,
-            'Away Team Defensive Rating Difference': away_dr_advantage,
-            'Home Team Offensive Rating Difference': home_or_advantage,
-            'Away Team Offensive Rating Difference': away_or_advantage,
-            'Home FG%': ((TeamStats(homeTeam))['field_goal_percentage'][0]),
-            'Away FG%': ((TeamStats(awayTeam))['field_goal_percentage'][0]),
-            'Home Assists P/G' : ((TeamStats(homeTeam))['assists'][0])/homeGamesPlayed,
-            'Away Assists P/G' : ((TeamStats(awayTeam))['assists'][0])/awayGamesPlayed,
-            'Home Turnovers P/G': ((TeamStats(homeTeam))['turnovers'][0])/homeGamesPlayed,
-            'Away Turnovers P/G': ((TeamStats(awayTeam))['turnovers'][0])/awayGamesPlayed
+            'Home Defensive Rating Difference': home_dr_advantage,
+            'Away Defensive Rating Difference': away_dr_advantage,
+            'Home Offensive Rating Difference': home_or_advantage,
+            'Away Offensive Rating Difference': away_or_advantage,
+            'Home Rebound Advantage': home_rb_advantage,
+            'Away Rebound Advantage' : away_rb_advantage,
+            'Home Three Point Advantage': home_three_advantage,
+            'Away Three Point Advantage': away_three_advantage,
+            'Home FG% Difference': home_fg_advantage,
+            'Away FG% Difference': away_fg_advantage,
+            'Home Assists/Turnover Ratio Difference' : home_at_advantage,
+            'Away Assists/Turnover Ratio Difference' : away_at_advantage,
+            'Home FGA Difference': home_fga_advantage,
+            'Away FGA Difference': away_fga_advantage,
         }
 
         all_stats.append(stats)
